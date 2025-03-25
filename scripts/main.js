7bdcbc357a6a2d684f6b476eb99483af67471be5
Antoine <gantoine.st@gmail.com>
2025-03-25 01:09:03 +0100

    added scroll on main page, blank fun page

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const main_title = document.getElementById("main_title");
+  const sub_title = document.getElementById("sub_title");
+  const options_buttons = document.querySelectorAll(".options");
+  // sub_title.addEventListener("mouseover", () => {
+  //   main_title.classList.add("active");
+  //   options_buttons.forEach(element => {
+  //     element.classList.add("active")
+  //
+  //   });
+  //
+  // })
+  // sub_title.addEventListener("mouseout", () => {
+  //   main_title.classList.remove("active");
+  // })
+})
