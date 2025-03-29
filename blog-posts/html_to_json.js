9a38150e87bb4ed91d43d9465cb1bc2af40782cb
Antoine <gantoine.st@gmail.com>
2025-03-29 16:11:16 +0100

    automatically adds title and description from the html to json file

--- blog-posts/html_to_json.js
+++ blog-posts/html_to_json.js
@@ -1,5 +1,6 @@
 import fs from "fs/promises"
 import path from "path"
+import { JSDOM } from "jsdom"
 
 async function main() {
   try {
@@ -8,7 +9,7 @@ async function main() {
     const OUTPUT_FILE = "./blog-posts/blog_posts.json"
 
     const htmlJsonContent = await htmlToJson(HTML_DIR, DNS_NAME)
-    await fs.writeFile(OUTPUT_FILE, JSON.stringify(htmlJsonContent, null, 2), 'utf8');
+    await fs.writeFile(OUTPUT_FILE, JSON.stringify(htmlJsonContent, null, ), 'utf8');
     console.log(`HTML file contents from ${HTML_DIR} written to ${OUTPUT_FILE} successfully.`)
   } catch (err) {
     console.log(err)
@@ -22,13 +23,26 @@ async function htmlToJson(htmlDir, dns) {
     // reading content of each file
     for (let index in htmlFiles) {
       const filePath = path.join(htmlDir, htmlFiles[index])
+
+      //url of the file using the dns name
       const url = path.join(dns, filePath).replace("https:/", "https://")
+
+      //file content
       const content = await fs.readFile(filePath, 'utf-8')
+      
+      //reading <title> and <summary> inside the html file
+      const dom = new JSDOM(content);
+      const document = dom.window.document;
+      const title = document.querySelector("header h1").textContent;
+      const summary = document.querySelector("header summary").textContent;
+
+      //file stats
       const stats = await fs.stat(filePath)
+
       htmlContents.push({
         "id": parseInt(index),
-        "title": "",
-        "description": "",
+        "title": title,
+        "description": summary,
         "content": content,
         "author": "Antoine Geiger",
         "url": url,
