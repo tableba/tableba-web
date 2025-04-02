7e6e398bd54f3cfeb0845e92dbeebb5686598ef2
Antoine <gantoine.st@gmail.com>
2025-04-03 01:39:23 +0200

    sound script but wrong format?

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -2,13 +2,38 @@ const SOUND_FILE_PATH = "https://tableba.com/src/music/sounds.json"
 
 document.addEventListener("DOMContentLoaded", async () => {
   const result = await fetchData(SOUND_FILE_PATH)
-  for (let soundPath of result) {
-    const soundFile = await fetch(soundPath)
-    console.log(soundFile)
-
-  }
-  console.log(result)
+  renderSounds(result)
 })
+
+function renderSounds(soundArr) {
+  let soundDiv = document.getElementById("soundDiv")
+  let frag = document.createDocumentFragment()
+  for (let sound of soundArr) {
+    let audio = document.createElement("audio")
+    audio.controls = true
+    let source = document.createElement("source")
+    const mime = getMimeType(sound)
+    source.src = sound
+    source.type = mime
+
+    audio.appendChild(source)
+    frag.appendChild(audio)
+  }
+  soundDiv.appendChild(frag)
+}
+
+function getMimeType(filePath) {
+    const extension = filePath.split('.').pop().toLowerCase(); // Extract file extension
+    const mimeTypes = {
+        'mp3': 'audio/mpeg',
+        'ogg': 'audio/ogg',
+        'wav': 'audio/wav',
+        'aac': 'audio/aac',
+        'flac': 'audio/flac'
+    };
+
+    return mimeTypes[extension] || 'audio/mpeg'; // Default to mp3 if unknown
+}
 
 async function fetchData(url) {
   const response = await fetch(url)
