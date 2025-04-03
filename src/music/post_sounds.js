9510f0c91fbb08464975061ce38e5c300d31f2ec
Antoine <gantoine.st@gmail.com>
2025-04-03 13:56:55 +0100

    corrected sound path again

--- src/music/post_sounds.js
+++ src/music/post_sounds.js
@@ -2,13 +2,13 @@ import fs from "fs/promises"
 import path from "path"
 import { JSDOM } from "jsdom"
 
-const DNS_NAME = "https://tableba.com"
 const AUDIO_DIR = "./src/music/assets"
+const RELATIVE_OUTPUT_DIR = "./music/assets"
 const OUTPUT_FILE = "./src/music/sounds.json"
 
 async function main() {
   try {
-    const soundArray = await readSouds(AUDIO_DIR, DNS_NAME)
+    const soundArray = await readSouds(RELATIVE_OUTPUT_DIR, AUDIO_DIR)
     await fs.writeFile(OUTPUT_FILE, JSON.stringify(soundArray, null, ), 'utf8');
     console.log(`Sound file contents from ${AUDIO_DIR} written to ${OUTPUT_FILE} successfully.`)
   } catch (err) {
@@ -16,12 +16,12 @@ async function main() {
   }
 }
 
-async function readSouds(audioDir, dnsName) {
+async function readSouds(outputDir, audioDir) {
   let files = []
   try {
     const audioFiles = await fs.readdir(audioDir)
     for (let soundPath of audioFiles) {
-      const filePath = path.join(dnsName, audioDir, soundPath)
+      const filePath = path.join(outputDir, soundPath)
       files.push(filePath)
     }
     return files
