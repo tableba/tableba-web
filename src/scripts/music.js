6bada6dd943baa0e64bf3ad47a721610cd68ecab
Antoine <gantoine.st@gmail.com>
2025-04-03 12:44:10 +0100

    sound path test

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -9,6 +9,7 @@ function renderSounds(soundArr) {
   let soundDiv = document.getElementById("soundDiv")
   let frag = document.createDocumentFragment()
   for (let sound of soundArr) {
+    console.log(sound)
     let audio = document.createElement("audio")
     audio.controls = true
     let source = document.createElement("source")
