16c0db40b6571f606f2537f7b7fdd421f6e88986
Antoine <gantoine.st@gmail.com>
2025-05-06 15:00:12 +0200

    sound link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dropdown = document.querySelectorAll(".dropdown")[0]
-  dropdown.addEventListener("click", () => {
-    dropdown.classList.toggle("open")
-  })
-})
-
-document.addEventListener("click", (event) => {
-  const dropdown = event.target.closest(".dropdown")
-  if (!dropdown) {
-    document.querySelectorAll(".dropdown.open")
-      .forEach(dropdown => dropdown.classList.remove("open"))
-  }
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("i see you.")
 })
