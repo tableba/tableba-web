a887f83b531ab00d5b6fe6d74042a345977da0e5
Antoine <gantoine.st@gmail.com>
2025-05-12 13:04:39 +0200

    env

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import { app } from './src/express.js'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
