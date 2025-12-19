8b60e067cdd5a2bf19dab2d4ea0096eba6d3783d
Antoine <gantoine.st@gmail.com>
2025-12-19 19:15:54 +0100

    update

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -1,27 +1,21 @@
-function ProjectCard({title, description, imageSrc, link}) {
-
+function ProjectCard({ title, description, imageSrc, link }) {
   return (
-    <section className="project-card grid grid-cols-[2fr_1fr] grid-rows-[auto_auto_2fr] gap-x-12 gap-y-6 w-95/100 m-10 group">
-        <h1 className="project-item col-start-1 row-start-1 row-span-2 2xl:text-6xl md:text-5xl font-bold" href={link}>{title}</h1>
-      <p className="project-item col-start-2 row-start-2 row-span-2 max-w-[32ch] ml-4 2xl:text-lg leading-relaxed">
+    <section className="project-card grid grid-cols-1 md:grid-cols-[2fr_1fr] grid-rows-auto gap-x-12 gap-y-6 w-11/12 m-10 group">
+      <h1 className="project-item col-span-1 md:col-start-1 md:row-start-1 md:row-span-2 2xl:text-6xl md:text-5xl font-bold">
+        {title}
+      </h1>
+      <p className="project-item col-span-1 md:col-start-2 md:row-start-2 md:row-span-2 max-w-[32ch] ml-0 md:ml-4 2xl:text-lg leading-relaxed">
         {description}
       </p>
       <a
         href={link}
         target="_blank"
         rel="noopener noreferrer"
-        className="col-start-1 row-start-3 ml-10 self-start"
+        className="col-span-1 md:col-start-1 md:row-start-3 ml-0 md:ml-10 self-start"
       >
-        <div
-          className="2xl:w-[800px] xl:w-[700px] w-[650px] aspect-[16/10]">
+        <div className="2xl:w-[800px] xl:w-[700px] w-full aspect-[16/10]">
           <img
-            className="
-            project-item w-full h-full object-cover origin-center
-            transition-transform
-            duration-500
-            ease-out
-            hover:scale-[1.03]
-            "
+            className="project-item w-full h-full object-cover origin-center transition-transform duration-500 ease-out hover:scale-[1.03]"
             src={imageSrc}
             alt={title}
           />
@@ -31,4 +25,5 @@ function ProjectCard({title, description, imageSrc, link}) {
   );
 }
 
-export default ProjectCard
+export default ProjectCard;
+
